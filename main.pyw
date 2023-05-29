f00ef805d1e7baded7a93486176573a8439f1e6d
Undeadamien <126392901+Undeadamien@users.noreply.github.com>
2023-05-29 23:38:59 +0200

    documentation and cleanning

--- main.pyw
+++ main.pyw
@@ -1,21 +1,19 @@
+"""Application displaying random images, with a timer"""
+
 from os import listdir
 from os.path import join
 
 from reference_window import ReferenceWindow
 from setup_window import SetupWindow
 
-
-IMAGE_PATH: str = r"C:\Users\Damien\Drawing\Photo_ref"
-IMAGE_EXTENSION: tuple[str] = (".jpg", ".png")
-IMAGES: list[str] = [
-    join(IMAGE_PATH, image_name)
-    for image_name in listdir(IMAGE_PATH)
-    if image_name.lower().endswith(IMAGE_EXTENSION)
-]
-assert IMAGES
+PATH = r"C:\Users\Damien\Drawing\Photo_ref"
+IMAGES = [join(PATH, file) for file in listdir(PATH) if file.endswith(".jpg")]
+assert IMAGES, "No files or folders found to process"
 
 
 def test_mediator(mediator):
+    """Test the conformity of the mediator before passing it to ReferenceWindow"""
+
     assert mediator[1] <= len(IMAGES)
     assert isinstance(mediator, (list, tuple))
     assert len(mediator) == 2
@@ -23,9 +21,11 @@ def test_mediator(mediator):
 
 
 def main():
+    """Run the different window"""
+
     mediator = []  # [time, amount]
 
-    SetupWindow(mediator, max_image=len(IMAGES)).run()
+    SetupWindow(mediator, len(IMAGES)).run()
     test_mediator(mediator)
     ReferenceWindow(mediator, IMAGES).run()
 
--- reference_window.py
+++ reference_window.py
@@ -1,9 +1,13 @@
+"""Module containing a personnalized Tk window"""
+
 from random import sample
 from tkinter import Button, Canvas, Label, Tk
 from PIL import Image, ImageTk
 
 
 class ReferenceWindow(Tk):
+    """Display x images during y minutes"""
+
     def __init__(
         self,
         mediator: list,
@@ -20,7 +24,7 @@ class ReferenceWindow(Tk):
 
         duration, amount, *_ = mediator
 
-        self.duration: int = duration * 60  # store the max duration in seconds
+        self.duration: int = duration * 60 + 1
         self.remaining_time: int = self.duration
         self.timer_update_call = None
         self.paused: bool = False
@@ -100,30 +104,31 @@ class ReferenceWindow(Tk):
         self.bind("<ButtonRelease-1>", self.stop_move)
 
     def convert_image(self, images: list) -> list[ImageTk.PhotoImage]:
+        """Convert and return, a list of image file into ImageTk.PhotoImage"""
+
         converted_images = []
 
         for image in images:
-            loaded_image = Image.open(f"{image}")
+            image = Image.open(image)
+            image.thumbnail(self.image_size)
 
-            width, height = loaded_image.size
-            resized_image = loaded_image.resize((width * 2, height * 2))
-            resized_image.thumbnail(self.image_size)
-
-            converted_images.append(ImageTk.PhotoImage(resized_image))
+            converted_images.append(ImageTk.PhotoImage(image))
 
         return converted_images
 
     def pause(self) -> None:
+        """Pause the timer cover the image"""
+
         self.paused = not self.paused
 
         if self.paused:
             self.after_cancel(self.timer_update_call)
             self.picture.grid_forget()
             self.cover.grid(row=1, column=0, columnspan=3)
-            self.cover.configure(
-                height=self.picture.winfo_height() - 2,
-                width=self.picture.winfo_width() - 2,
-            )
+
+            height = self.picture.winfo_height() - 2
+            width = self.picture.winfo_width() - 2
+            self.cover.configure(height=height, width=width)
 
         else:
             self.cover.grid_forget()
@@ -131,18 +136,20 @@ class ReferenceWindow(Tk):
             self.timer_update_call = self.after(500, self.update_timer)
 
     def update_image(self) -> None:
+        """Pass to the next image and reset the timer"""
+
         self.current_image += 1
 
-        if self.current_image > len(self.images) - 1:
+        if self.current_image >= len(self.images):
             self.destroy()
 
         else:
             self.picture.configure(image=self.images[self.current_image])
-            self.after_cancel(self.timer_update_call)
-            self.remaining_time = self.duration
-            self.update_timer()
 
     def update_timer(self) -> None:
+        """Call it self recursively, updating the time and the timer label"""
+
+        self.remaining_time -= 1
         minutes, seconds = divmod(self.remaining_time, 60)
 
         self.timer.configure(
@@ -153,27 +160,34 @@ class ReferenceWindow(Tk):
             highlightthickness=2,
         )
 
-        if not self.paused:
-            self.remaining_time -= 1
-
-        if self.remaining_time >= -1:
+        if self.remaining_time >= 0:
             self.timer_update_call = self.after(1000, self.update_timer)
 
         else:
+            self.after_cancel(self.timer_update_call)
+            self.remaining_time = self.duration
+            self.update_timer()
             self.update_image()
 
-    def run(self):
+    def run(self) -> None:
+        """Wrapper for update_timer and mainloop"""
+
         self.update_timer()
         self.mainloop()
 
     def start_move(self, event) -> None:
+        """Store the position of the mouse relative to the window"""
+
         self.start_x, self.start_y = event.x, event.y
 
     def do_move(self, event) -> None:
-        self.geometry(
-            f"+{self.winfo_x() + event.x - self.start_x}"
-            + f"+{self.winfo_y() + event.y - self.start_y}"
-        )
+        """Move the window"""
+
+        pos_x = self.winfo_x() + event.x - self.start_x
+        pos_y = self.winfo_y() + event.y - self.start_y
+        self.geometry(f"+{pos_x}+{pos_y}")
 
     def stop_move(self, _) -> None:
+        """Reset self.start_x and self.start_y"""
+
         self.start_x, self.start_y = None, None
