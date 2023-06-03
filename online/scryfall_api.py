3511a14c868a738ac7a49ecb7f51e07baf701d36
Undeadamien <126392901+Undeadamien@users.noreply.github.com>
2023-06-03 11:09:37 +0200

    implementation of scryfall api, to get mtg illustration online

---
+++ online/scryfall_api.py
@@ -0,0 +1,23 @@
+import io
+
+import requests
+
+url = "https://api.scryfall.com/cards/random"
+headers = {"Content-Type": "application/json"}
+
+
+def request_image(amount):
+    images = []
+
+    while len(images) < amount:
+        res = requests.get(url, headers=headers, timeout=30).json()
+
+        try:
+            image_url = res["image_uris"]["art_crop"]
+        except KeyError:
+            continue
+
+        image = requests.get(image_url, timeout=30).content
+        images.append(io.BytesIO(image))
+
+    return images
